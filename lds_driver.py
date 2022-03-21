f01475d5dd0627ca882e30242509ca7b0868626a
ZTX <Tianxiang.18@intl.zju.edu.cn>
2022-03-21 18:50:52 +0800

    zs-happy

--- lds_driver.py
+++ lds_driver.py
@@ -3,35 +3,45 @@ import serial
 def int_to_bytes(x: int) -> bytes:
     return x.to_bytes((x.bit_length() + 7) // 8, 'big')
 
+def bytes_to_int(bytes):
+    result = 0
+    for b in bytes:
+        result = result * 256 + int(b)
+    return result
+
 def lds_poll(ser):
     got_scan = False
     raw_bytes = [0] * 2520
-    res = [0] * 360
+    res = [-1] * 360
     # print(raw_bytes)
     start_count, good_sets, motor_speed = 0, 0, 0
     while (~got_scan):
-        raw_bytes[start_count] = ser.read(1)
+        raw_bytes[start_count] = bytes_to_int(ser.read(1))
         # print(ser.read(1))
         # if (raw_bytes[start_count] == b'\xfa'):
         #     print("nms")
         if (start_count == 0):
-            if (raw_bytes[start_count] == b'\xfa'):
+            if (raw_bytes[start_count] == bytes_to_int(b'\xfa')):
                 start_count = 1
                 # print("gdl")
         elif (start_count == 1):
-            if (raw_bytes[start_count] == b'\xa0'):
+            if (raw_bytes[start_count] == bytes_to_int(b'\xa0')):
                 # print("gdl2")
                 start_count = 0
                 got_scan = True
                 data = ser.read(2518)
                 for i in range(2518):
                     raw_bytes[2+i] = data[i]
-                
+                #print(int_to_bytes(data[0]))
+                #print(data)
                 for i in range(0,2520,42):
-                    if (raw_bytes[i] == b'\xfa' and raw_bytes[i+1] == (b'\xa0' + int_to_bytes(int(i / 42)))):
+                    #print(bytes_to_int(int_to_bytes(raw_bytes[i+1])), ' ', bytes_to_int(b'\xa0') + int(i / 42))
+                    #print(int_to_bytes(raw_bytes[i]) == b'\xfa', ' ', int_to_bytes(raw_bytes[i+1]) == (b'\xa0' + int_to_bytes(int(i / 42))))
+                    if (int_to_bytes(raw_bytes[i]) == b'\xfa' and raw_bytes[i+1] == bytes_to_int(b'\xa0') + (int(i / 42))):
+                    
                         good_sets+=1
                         motor_speed += (raw_bytes[i+3]<<8) + raw_bytes[i+2]
-                        s=(raw_bytes[i+3]<<8|raw_bytes[i+2])/10
+                        s= (raw_bytes[i+3] << 8 | raw_bytes[i+2]) / 10
                         
                         for j in range(i+4, i+40, 6):
                             index = 6*(i/42) + (j-4-i)/6
@@ -39,10 +49,15 @@ def lds_poll(ser):
 
                             intensity = (byte1 << 8) + byte0
                             rge = (byte3 << 8) + byte2
-                            res[359-int(index)] = rge / 1000.0
+                            #res[359-int(index)] = rge / 1000.0
+                            res[int(index)] = rge / 1000.0
+                            #print('r[%d]=%f' %(int(index),rge / 1000.0))
+                            print(res[100])
+
             else:
                 start_count = 0
-        print(res)             
+            
+        #print(res)             
     return 
 def lds_driver_init():
     # Setting up baud rate and io service
