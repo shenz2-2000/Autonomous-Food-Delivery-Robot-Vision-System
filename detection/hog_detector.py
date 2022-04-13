a7280f09e363a12ab29a4a93fde5b74bf2c1120a
Mo-Kanya <kanya.18@intl.zju.edu.cn>
2022-04-14 02:36:05 +0800

    hog detector is far from good

---
+++ detection/hog_detector.py
@@ -0,0 +1,59 @@
+# The hog detector only works when full body appears, and is slow (using CPU)
+
+# import the necessary packages
+import numpy as np
+import cv2
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv2.CAP_PROP_FPS, 30)
+
+# open webcam video stream
+cv2.startWindowThread()
+
+# the output will be written to output.avi
+out = cv2.VideoWriter(
+    'output.mp4',
+    cv2.VideoWriter_fourcc(*'mp4v'),
+    25.,
+    (640, 480))
+
+while (True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # resizing for faster detection
+    frame = cv2.resize(frame, (640, 480))
+    # using a greyscale picture, also for faster detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+    # detect people in the image
+    # returns the bounding boxes for the detected objects
+    boxes, weights = hog.detectMultiScale(frame, winStride=(8, 8))
+
+    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
+    for (xA, yA, xB, yB) in boxes:
+        # display the detected boxes in the colour picture
+        cv2.rectangle(frame, (xA, yA), (xB, yB),
+                      (0, 255, 0), 2)
+
+    # Write the output video
+    out.write(frame.astype('uint8'))
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+# and release the output
+out.release()
+# finally, close the window
+cv2.destroyAllWindows()
+cv2.waitKey(1)
